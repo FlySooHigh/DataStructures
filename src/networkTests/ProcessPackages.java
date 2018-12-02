5fba29c697d2df4f374ce0855df7eb82658645ba
FlySooHigh <flysoohigh63@gmail.com>
2018-12-02 18:03:17 +0400

    Added solution for network assignment

--- src/networkTests/ProcessPackages.java
+++ src/networkTests/ProcessPackages.java
@@ -1,19 +1,48 @@
-package network;
+package networkTests;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class ProcessPackages {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private static final int numberOfTests = 22;
 
-        int bufferSize = scanner.nextInt();
-        Buffer buffer = new Buffer(bufferSize);
+    public static void main(String[] args) throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+        String path = "tests\\network\\";
 
-        ArrayList<Request> requests = readQueries(scanner);
-        ArrayList<Response> responses = processRequests(requests, buffer);
-        printResponses(responses);
+        for (int i = 1; i <= numberOfTests; i++) {
+            String fileName = String.format("%02d", i);
+            Scanner scanner = new Scanner(new File(path + fileName));
+            int bufferSize = scanner.nextInt();
+            Buffer buffer = new Buffer(bufferSize);
+
+            ArrayList<Request> requests = readQueries(scanner);
+            ArrayList<Response> responses = processRequests(requests, buffer);
+
+            StringBuilder sb = new StringBuilder();
+            StringBuilder actualResult = printResponses(responses, sb);
+
+            scanner = new Scanner(new File(path + fileName + ".a"));
+            StringBuilder expectedResult = new StringBuilder();
+            while (scanner.hasNext()) {
+                expectedResult.append(scanner.nextInt()).append("\n");
+            }
+
+            if (sb.toString().equals(expectedResult.toString())) {
+                System.out.println("\033[32m" + "Test #" + i + " passed" + "\033[0m");
+            } else {
+                System.out.println("\033[31m" + "Test #" + i + " failed" + "\033[0m");
+                BufferedWriter writer = new BufferedWriter(new FileWriter("actualResult"));
+                writer.write(actualResult.toString());
+                writer.close();
+//                break;
+            }
+        }
     }
 
     private static ArrayList<Request> readQueries(Scanner scanner) {
@@ -35,14 +64,15 @@ class ProcessPackages {
         return responses;
     }
 
-    private static void printResponses(ArrayList<Response> responses) {
+    private static StringBuilder printResponses(ArrayList<Response> responses, StringBuilder sb) {
         for (Response response : responses) {
             if (response.dropped) {
-                System.out.println(-1);
+                sb.append(-1 + "\n");
             } else {
-                System.out.println(response.startTime);
+                sb.append(response.startTime).append("\n");
             }
         }
+        return sb;
     }
 
 }
@@ -60,12 +90,12 @@ class Buffer {
         // write your code here
         Integer removedElem = null;
         if (!finishTime.isEmpty()) {
-            while (finishTime.get(0) <= request.getArrivalTime()) {
-                removedElem = finishTime.remove(0);
-                if (finishTime.isEmpty()) {
-                    break;
+                while (finishTime.get(0) <= request.getArrivalTime()) {
+                    removedElem = finishTime.remove(0);
+                    if (finishTime.isEmpty()) {
+                        break;
+                    }
                 }
-            }
         }
         if (finishTime.size() < maxSize) {
             if (finishTime.size() == 0) {
